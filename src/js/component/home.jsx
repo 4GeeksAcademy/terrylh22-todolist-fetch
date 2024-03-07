493b4bbb142bdb233422962c62d99860926e9d4e
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-03-07 01:58:31 +0000

    worked on logic

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
+import List from "./list.jsx";
 
 //create your first component
 const Home = () => {
+	const [textArray, setTextArray] = useState([]);
+	const [textBox, setTextBox] = useState("");
+
+	function handleOnEnter(event) {
+		if (event.key === "Enter") {
+			setTextArray(prevTextArray => [...prevTextArray, textBox]);
+			setTextBox("");
+		}
+	}
+
+	function handleOnChange(event) {
+		setTextBox(event.target.value);
+	}
+
+	useEffect(() => {
+		console.log(textArray);
+	}, [textArray])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<input type="text" value={textBox} onKeyDown={handleOnEnter} onChange={handleOnChange}></input>
+			<List />
+		</>
 	);
 };
 
---
+++ src/js/component/list.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Item from './item.jsx';
+
+function List() {
+    return (
+        <ol>
+            <Item text="hello world"/>
+        </ol>
+    )
+}
+
+export default List;
