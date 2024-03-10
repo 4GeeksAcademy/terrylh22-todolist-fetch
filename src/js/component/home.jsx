0ee703ad9e9b4ff9dfe7264c1348398e2bb5efed
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-03-10 23:15:59 +0000

    added styling changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import List from "./list.jsx";
+import Item from "./item.jsx";
 
 //create your first component
 const Home = () => {
@@ -17,15 +17,24 @@ const Home = () => {
 		setTextBox(event.target.value);
 	}
 
+	function handleDelete(index) {
+		setTextArray(prevTextArray => prevTextArray.filter((_, i) => i !== index));
+	}
+
 	useEffect(() => {
 		console.log(textArray);
 	}, [textArray])
 
 	return (
-		<>
-			<input type="text" value={textBox} onKeyDown={handleOnEnter} onChange={handleOnChange}></input>
-			<List />
-		</>
+		<div className="myList">
+			<h1 className="heading">todo list</h1>
+			<ul className="list-group group">
+				<input type="text" value={textBox} onKeyDown={handleOnEnter} onChange={handleOnChange} placeholder="What's the next task?"></input>
+				{textArray.map((item, index) => (
+					<Item key={index} text={item} onDelete={(() => handleDelete(index))}></Item>
+				))}
+			</ul>
+		</div>
 	);
 };
 
--- src/js/component/item.jsx
+++ src/js/component/item.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import '../../styles/index.css'
 
-function Item() {
+
+function Item(props) {
+
+    const handleDelete = () => {
+        if (props.onDelete) {
+            props.onDelete();
+        }
+    };
+
     return (
-        <li>"Hello World"</li>
+        <li className="list-group-item group listItem">
+            <span>{props.text}</span> 
+            <i class="fa fa-trash trash-icon" onClick={handleDelete}></i>
+        </li>
     )
 }
 
